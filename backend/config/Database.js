515b121976290a3f5327d2e6d8409459e3331a6d
demoon19 <150596597+demoon19@users.noreply.github.com>
2025-03-23 23:06:33 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("tcc5", "root", "", {
-  host: "34.72.93.57",
+const db = new Sequelize("docker", "root", "", {
+  host: "34.60.39.112",
   dialect: "mysql",
 });
 
